1bbaf6df054bb6cd1cf66339d6bc2c09816563a8
Austine <austine.amah@gmail.com>
2022-05-10 13:23:20 +0100

    User Model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :transactions
+
+  validates_presence_of :password, :email
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
   subject { 
-    user = User.create!(first_name: "Austine")
+    user = User.create!(first_name: "Austine", last_name: "Amah", email: "austine", password: "123456")
     described_class.new(
       user: user,
       input_amount: 5000*100,
@@ -12,32 +12,35 @@ RSpec.describe Transaction, type: :model do
     ) 
   }
 
-  it "should belong to a user" do
-    expect(described_class.reflect_on_association(:user).macro).to eq(:belongs_to) 
-  end
+  context 'before transactions is created' do  
+    it "must be associated to a User" do
+      expect(described_class.reflect_on_association(:user).macro).to eq(:belongs_to) 
+    end
 
-  it "is valid with all valid attributes" do
-    expect(subject).to be_valid
-  end
+    it "must have all required attributes present" do
+      expect(subject).to be_valid
+    end
 
-  it "is not valid without an input_amount" do
-    subject.input_amount = nil
-    expect(subject).to_not be_valid
-  end
+    it "cannot be valid without an input_amount" do
+      subject.input_amount = nil
+      expect(subject).to_not be_valid
+    end
 
-  it "is not valid without an input_currency" do
-    subject.input_currency = nil
-    expect(subject).to_not be_valid
-  end
+    it "cannot be valid without an input_currency" do
+      subject.input_currency = nil
+      expect(subject).to_not be_valid
+    end
 
-  it "is not valid without an output_amount" do
-    subject.output_amount = nil
-    expect(subject).to_not be_valid
-  end
+    it "cannot be  valid without an output_amount" do
+      subject.output_amount = nil
+      expect(subject).to_not be_valid
+    end
 
-  it "is not valid without an output_currency" do
-    subject.output_currency = nil
-    expect(subject).to_not be_valid
-  end
+    it "cannot be  valid without an output_currency" do
+      subject.output_currency = nil
+      expect(subject).to_not be_valid
+    end
  
+  end
+
 end
