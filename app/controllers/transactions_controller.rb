6fd14ea5abe81e17c54744e4d90633e498db9b3a
Austine <austine.amah@gmail.com>
2022-05-11 17:06:54 +0100

    Transaction show

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,6 @@
 class TransactionsController < ApplicationController
   before_action :authenticate_user, only: [:create]
+  before_action :set_transaction, only: [:show]
 
   def index
     @transactions = Transaction.all
@@ -8,30 +9,23 @@ class TransactionsController < ApplicationController
   def create
     begin
       ActiveRecord::Base.transaction do
+        output_amount = CurrencyConverter.call( transaction_params[:input_amount], transaction_params[:input_currency], transaction_params[:output_currency])
+        @transaction = current_user.transactions.create!( transaction_params.merge( output_amount: output_amount["result"] ))
 
-        output_amount = CurrencyConverter.call(
-          params[:transaction][:input_amount], 
-          params[:transaction][:input_currency], 
-          params[:transaction][:output_currency]
-          )
-
-        @transaction = current_user.transactions.create!(
-            transaction_params.merge(
-              output_amount: output_amount["result"]
-            )
-          )
-
-        render :show, status: 200 
-
+        render :show, status: 200
       end
     rescue StandardError => e
-
       render json: { errors: e }, status: 400
-
     end
   end
 
+  def show; end
+
   private
+
+  def set_transaction
+    @transaction = Transaction.find(params[:id])
+  end
 
   def transaction_params
     params.require(:transaction).permit(:input_amount, :input_currency, :output_amount, :output_currency)
--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Transactions", type: :request do
-  let(:transaction_params) { { "input_amount" => 5000, "input_currency" => "USD", "output_currency" => "NGN"} }
-
-  user = User.create!(
-    password: "123456", 
-    email: "kalel.amah@gmail.com"
-  )
-
-  token = TokenGenerator.call( {sub: user.id} )
-
-  before do
-    Transaction.create(transaction_params.merge( user: user)) 
-  end
-
   
+  let(:transaction_params) { { "input_amount" => 5000, "input_currency" => "USD", "output_currency" => "NGN"} }
+  let(:user) { User.create!(password: "123456", email: "kalel.amah@gmail.com")}
+  let(:token){ TokenGenerator.call( {sub: user.id} ) }
+
+  subject { 
+    Transaction.create(transaction_params.merge( user: user)) 
+  }
+
+
   describe "GET /index" do
+    before do
+      Transaction.create(transaction_params.merge( user: user)) 
+    end
+
     it "responds with all transactions" do
       get "/transactions", headers: { "Authorization": "Bearer #{token}" }
       expect(response).to have_http_status(:ok)
@@ -28,8 +28,14 @@ RSpec.describe "Transactions", type: :request do
       post "/transactions", params: { transaction: transaction_params }, headers: { "Authorization": "Bearer #{token}" }
       expect(response).to have_http_status(:ok)
       expect(response.body).to include("input_amount")
+    end
+  end
 
-      # puts response.body
+  describe "GET /show" do
+    it "responds with a transaction" do
+      get "/transactions/#{subject.id}"
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include("input_amount")
     end
   end
 
